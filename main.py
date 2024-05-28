769a731697fa9618c1f78777188450ba30e96d65
LenaMaistrenko <istlauder@gmail.com>
2024-05-28 21:25:05 +0300

    Add BookStore

--- main.py
+++ main.py
@@ -44,3 +44,70 @@ class EntityFactory:
     @staticmethod
     def create_sale(employee, book, sale_date, actual_sale_price):
         return Sale(employee, book, sale_date, actual_sale_price)
+class Observable:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer):
+        self._observers.append(observer)
+
+    def detach(self, observer):
+        self._observers.remove(observer)
+
+    def notify(self):
+        for observer in self._observers:
+            observer.update()
+
+class EmployeeManager(Observable):
+    def __init__(self):
+        super().__init__()
+        self.employees = []
+
+    def add_employee(self, employee):
+        self.employees.append(employee)
+        self.notify()
+
+    def remove_employee(self, employee):
+        self.employees.remove(employee)
+        self.notify()
+
+class BookManager(Observable):
+    def __init__(self):
+        super().__init__()
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        self.notify()
+
+    def remove_book(self, book):
+        self.books.remove(book)
+        self.notify()
+class SaleManager(Observable):
+    def __init__(self):
+        super().__init__()
+        self.sales = []
+
+    def make_sale(self, employee, book, sale_date, actual_sale_price):
+        sale = EntityFactory.create_sale(employee, book, sale_date, actual_sale_price)
+        self.sales.append(sale)
+        self.notify()
+
+class Bookstore:
+    _instance = None
+
+    def __init__(self):
+        if Bookstore._instance is not None:
+            raise Exception("This class is a singleton!")
+        else:
+            self.employee_manager = EmployeeManager()
+            self.book_manager = BookManager()
+            self.sale_manager = SaleManager()
+            self.report_strategy = None
+            Bookstore._instance = self
+
+    @staticmethod
+    def get_instance():
+        if Bookstore._instance is None:
+            Bookstore()
+        return Bookstore._instance
